7f6aab06f78e4f8ad3c7b33bdee0a1a8e24d2a6c
Kamini Sahu <sahunami843525@gmail.com>
2026-04-29 17:14:28 +0530

    done

--- react/myapp/src/CoreApi.js
+++ react/myapp/src/CoreApi.js
@@ -1,6 +1,7 @@
 import axios from "axios"; 
 
-const API_URL = "http://127.0.0.1:8000/";
+// const API_URL = "http://127.0.0.1:8000/";
+const API_URL = process.env.REACT_APP_API_URL;
 
 // Fetch login data
 export const GEtLoginData = async () => {
